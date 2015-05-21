c4d8167beb8a8c28f15c70fb91bff6dded143589
Dan Vestergaard <dan@dancoder.com>
2015-05-21 12:36:40 +0200

    SloackLoadingIndicator project

---
+++ SlackLoadingIndicator/SlackLoadingIndicator/SlackLoadingIndicator.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace SlackLoader
+{
+  /// <summary>
+  /// Interaction logic for SlackLoadingIndicator.xaml
+  /// </summary>
+  public partial class SlackLoadingIndicator
+  {
+    public SlackLoadingIndicator()
+    {
+      InitializeComponent();
+
+      IsVisibleChanged += SlackLoadingIndicator_IsVisibleChanged;
+    }
+
+    private void SlackLoadingIndicator_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+      StartStopStoryboard(IsVisible);
+    }
+
+    private void StartStopStoryboard(bool start)
+    {
+      var lineAnimationStoryboard = FindResource("LineAnimationStoryboard") as Storyboard;
+      if (lineAnimationStoryboard != null)
+      {
+        if (start)
+        {
+          lineAnimationStoryboard.Begin();
+        }
+        else
+        {
+          lineAnimationStoryboard.Stop();
+        }
+      }
+    }
+  }
+}
