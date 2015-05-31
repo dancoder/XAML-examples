2f1556c76b0c2cb66cc32d366558c3f88bae7743
Dan Vestergaard <dan@dancoder.com>
2015-05-31 18:35:47 +0200

    Wizard project

---
+++ Wizard/Wizard/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Wizard
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void OnClickToBasket(object sender, RoutedEventArgs e)
+    {
+      CheckoutTabControl.SelectedIndex = 0;
+    }
+
+    private void OnClickToBillingInformation(object sender, RoutedEventArgs e)
+    {
+      CheckoutTabControl.SelectedIndex = 1;
+    }
+
+    private void OnClickToPaymentDetails(object sender, RoutedEventArgs e)
+    {
+      CheckoutTabControl.SelectedIndex = 2;
+    }
+
+    private void OnClickToBuy(object sender, RoutedEventArgs e)
+    {
+      CheckoutTabControl.SelectedIndex = 3;
+    }
+  }
+}
