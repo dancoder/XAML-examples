5790e9c5b8a211b9b58477b6901387a2c3a9d039
Dan Vestergaard <dan@dancoder.com>
2015-06-13 17:47:43 +0200

    Added the ElasticSlackLoadingIndicator project

---
+++ SlackLoadingIndicator/ElasticSlackLoadingIndicator/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SlackLoader
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+}
---
+++ SlackLoadingIndicator/ElasticSlackLoadingIndicator/ElasticSlackLoadingIndicator.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace SlackLoader
+{
+  /// <summary>
+  /// Interaction logic for ElasticSlackLoadingIndicator.xaml
+  /// </summary>
+  public partial class ElasticSlackLoadingIndicator
+  {
+    public ElasticSlackLoadingIndicator()
+    {
+      InitializeComponent();
+
+      IsVisibleChanged += SlackLoadingIndicator_IsVisibleChanged;
+    }
+
+    private void SlackLoadingIndicator_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+      StartStopStoryboard(IsVisible);
+    }
+
+    private void StartStopStoryboard(bool start)
+    {
+      var lineAnimationStoryboard = FindResource("LineAnimationStoryboard") as Storyboard;
+      if (lineAnimationStoryboard != null)
+      {
+        if (start)
+        {
+          lineAnimationStoryboard.Begin();
+        }
+        else
+        {
+          lineAnimationStoryboard.Stop();
+        }
+      }
+    }
+  }
+}
